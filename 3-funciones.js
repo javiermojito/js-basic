314acaeb20c68653986ad24f6ff0632eaec90245
Javier Collazos <jcollazosgomez@gmail.com>
2020-10-01 16:49:19 -0500

    Se agrego funciones

---
+++ 3-funciones.js
@@ -0,0 +1,36 @@
+//Declarativas
+function saludar(){
+   console.log("Hola");
+}
+
+//Expresión
+var saludarVersion2 = function (){
+    console.log("Hola soy la version 2");
+}
+
+saludarVersion2();
+
+//----//
+
+function suma(param1,param2){
+    return param1 + param2;
+}
+
+console.log(suma(5,"10"));
+
+function saludarEstudiante(nombreEstudiante){
+    console.log("Bienvenido estudiante " + nombreEstudiante);
+}
+
+saludarEstudiante("José");
+
+//PLANTILLAS LITERALES
+function saludarEstudiante(nombreEstudiante){
+    console.log(`Hola estudiante ${nombreEstudiante}, te recuerdo que el nombre es ${nombreEstudiante}`);
+}
+
+saludarEstudiante("Javier");
+
+function sumar(a,b){
+    return a+b;
+}
