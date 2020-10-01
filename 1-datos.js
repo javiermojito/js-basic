438c0ce1025b5c1d60275f495704ffff5231eeac
Javier Collazos <jcollazosgomez@gmail.com>
2020-10-01 16:18:58 -0500

    Agregamos datos

---
+++ 1-datos.js
@@ -0,0 +1,23 @@
+//Primitivos
+40 
+
+"Javier"
+'Javier'
+
+//boolean
+true //-> Verdadero
+false // -> Falso
+
+
+null
+undefined
+
+//tipo objeto
+
+//Arreglo
+[1,2,3];
+
+//objeto
+{
+    nombre: "Javier";
+}
