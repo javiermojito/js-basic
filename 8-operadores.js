69a8e9fc39235fb01d56c6366492f9297d7d3db9
Javier Collazos <jcollazosgomez@gmail.com>
2020-10-05 16:23:45 -0500

    operadores

---
+++ 8-operadores.js
@@ -0,0 +1,46 @@
+//Aritméticos
+5 + 2
+7 - 8
+5 * (5-2)
+5 / 9
+10 % 2 
+2 ** 4
+
+2*2*2*2*2 == 2**5
+
+"Javier" + " Collazos"; //Concatenación
+
+!true // Negación
+
+
+//Operador de asignacion
+var a = 1;
+
+//Comparación  -> Hablamos de resultados booleans 
+//number     //string
+  3      ==  "3";     //True
+
+//number     //string
+  3      === "3"; //false
+
+5 < 5 //false
+5 > 3 //true
+5 <= 5 //true
+5 >= 6 //false
+
+
+//AND
+(true && false) //AND --> false
+(true && true) //AND --> true
+(1 && 34) //AND --> true
+
+//OR
+(a || b) // OR
+(true || false) // OR --> true
+(false || false) // OR --> false
+
+
+var edad = 20;
+edad++; //Solo incrementa en 1
+edad *= 2; // edad = edad * 2;
+edad -= 50; // edad = edad - 50;
