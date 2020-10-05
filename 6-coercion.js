df3d08691d8b033cfc325e09c322957256edec85
Javier Collazos <jcollazosgomez@gmail.com>
2020-10-05 15:56:01 -0500

    Coerción

---
+++ 6-coercion.js
@@ -0,0 +1,14 @@
+// Coerción implícita
+var a = 1 + "2"; // 12 -- Casting
+console.log(typeof a);
+
+var b = 4 * "2";
+console.log(typeof b);
+
+//Coerción explícita
+var c = 15;
+//var d = c + ""; //Casting de number a String
+
+var d = String(c); //Casting de number(c) a String(d)
+
+var e = Number(d); //Casting de String(d) a number(e)
